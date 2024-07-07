bb4ac93cf6646d5e1ac2ebb5586fab9314047d27
Aven Arlington <aven.arlington@gmail.com>
2024-07-07 13:55:06 -0700

    Updating script to use for each style loop

--- apply_config.sh
+++ apply_config.sh
@@ -1,34 +1,17 @@
 #!/bin/bash
+set -v
+
 # store the current dir
 CUR_DIR=$(pwd)
-echo $CUR_DIR
+
 # Store the default .config file location
 CFG=$HOME/.config
-echo "Copying configuration files from the repository into ~/.config..."
 
-# Neovim configuration
-NVIM="/nvim"
-NVIM_CFG=$CUR_DIR$NVIM
-printf "Removing: %s\n" $CFG$NVIM
-rm -rf $CFG$NVIM
-printf "Copy: %s \nInto: %s\n" $NVIM_CFG $CFG
-cp -r $NVIM_CFG $CFG
-
-# Alacritty configuration
-ALACRITTY="/alacritty"
-ALACRITTY_CFG=$CUR_DIR$ALACRITTY
-printf "Removing: %s\n" $CFG$ALACRITTY
-rm -rf $CFG$ALACRITTY
-printf "Copy: %s \nInto: %s\n" $ALACRITTY_CFG $CFG
-cp -r $ALACRITTY_CFG $CFG
-
-# Zellij configuration
-ZELLIJ="/zellij"
-ZELLIJ_CFG=$CUR_DIR$ZELLIJ
-printf "Removing: %s\n" $CFG$ZELLIJ
-rm -rf $CFG$ZELLIJ
-printf "Copy: %s \nInto: %s\n" $ZELLIJ_CFG $CFG
-cp -r $ZELLIJ_CFG $CFG
+for VAR in "/nvim" "/alacritty" "/zellij"; do
+  NEW_CFG=$CUR_DIR$VAR
+  rm -rf $CFG$VAR
+  cp -r $NEW_CFG $CFG$VAR
+done
 
 # lets get back to the CUR_DIR
 cd $CUR_DIR
