9945da55668de1cc121cc2214b4d369d85598c51
Aven Arlington <aven.arlington@gmail.com>
2025-01-13 14:00:07 -0800

    Updating initial setup script and adding autoinstall for ubuntu

--- initial_device_setup/create_repos_file_structure.sh
+++ initial_device_setup/create_repos_file_structure.sh
@@ -2,6 +2,10 @@
 set -v
 
 mkdir $HOME/repos
-mkdir $HOME/repos/dev
-mkdir $HOME/repos/libraries
-mkdir $HOME/repos/tools
+mkdir $HOME/repos/public
+mkdir $HOME/repos/private
+mkdir $HOME/repos/third_party
+mkdir $HOME/repos/sandboxes
+mkdir $HOME/repos/games
+mkdir $HOME/repos/notebooks
+
